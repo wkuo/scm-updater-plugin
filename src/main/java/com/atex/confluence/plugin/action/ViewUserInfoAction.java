8fe002d6de1356e068e1888c7fae36d3837b424e
Pau Kiat Wee <paukiatwee@gmail.com>
2012-02-02 17:41:53 +0800

    first migrate

---
+++ src/main/java/com/atex/confluence/plugin/action/ViewUserInfoAction.java
@@ -0,0 +1,15 @@
+package com.atex.confluence.plugin.action;
+
+import com.atlassian.confluence.user.actions.ViewUserProfileAction;
+
+public class ViewUserInfoAction extends ViewUserProfileAction {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -9154238758008353895L;
+    
+    
+    
+
+}
